72033ea9de1b1d2b8585acaaafcc75c8030d5e80
mhigley <mhigley@bitwiseindustries.com>
2023-01-17 16:54:40 -0800

    page.js update

--- src/routes/newsroom/+page.js
+++ src/routes/newsroom/+page.js
@@ -1,4 +1,4 @@
-import { PUBLIC_LOCAL_WP_API_URL } from '$env/static/public';
+import { PUBLIC_LIVE_WP_API_URL } from '$env/static/public';
 
 
 export const load = async ({ fetch }) => {
@@ -27,7 +27,7 @@ export const load = async ({ fetch }) => {
           }
         `;
         
-        const storiesRes = await fetch(PUBLIC_LOCAL_WP_API_URL, {
+        const storiesRes = await fetch(PUBLIC_LIVE_WP_API_URL, {
             method: 'POST',
             headers: {
                 'Content-Type' : 'application/json'
