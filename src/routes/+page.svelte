aa1740bd3b091239e65feb80d9dfd3746f91c507
mhigley <mhigley@bitwiseindustries.com>
2023-01-26 22:24:09 -0800

    navigation component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-    import Test from '$lib/Test.svelte';
-    import '$db/start';
+    // import Test from '$lib/Test.svelte';
+    // import '$db/start';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Test />
---
+++ src/routes/Navigation.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { page } from '$app/stores';
+    $: routeId = $page.route.id;
+
+    const primaryLinks = [
+        {
+            primaryTitle: 'Services',
+            primaryHref: '/services',
+            secondaryLinks: []
+        },
+        {
+            primaryTitle: 'About',
+            primaryHref: '/about',
+            secondaryLinks: [
+                {
+                    secondaryTitle: 'First',
+                    secondaryHref: '/first'
+                },
+                {
+                    secondaryTitle: 'Second',
+                    secondaryHref: '/second'
+                }
+            ]
+        },
+        {
+            primaryTitle: 'Spaces',
+            primaryHref: '/spaces',
+            secondaryLinks: [
+                {
+                    secondaryTitle: 'Third',
+                    secondaryHref: '/third'
+                }
+            ]
+        },
+        {
+            primaryTitle: 'Newsroom',
+            primaryHref: '/newsroom',
+            secondaryLinks: []
+        }
+    ];
+
+
+</script>
+
+<nav>
+    {#if primaryLinks.length}
+    <ul>
+        {#each primaryLinks as {primaryTitle, primaryHref, secondaryLinks}}
+        <li><a href={primaryHref} class:active={routeId == primaryHref} title={primaryTitle}>{primaryTitle}</a>
+            {#if secondaryLinks.length}
+            <ul>
+                {#each secondaryLinks as {secondaryTitle, secondaryHref}}
+                <li><a href={primaryHref}{secondaryHref} class:active={routeId == secondaryHref} title={secondaryTitle}>{secondaryTitle}</a></li>
+                {/each}
+            </ul>
+            {/if}
+        </li>
+        {/each}
+    </ul>
+    {/if}
+</nav>
+
+<style>
+    .active {
+        color: red;
+    }
+</style>
